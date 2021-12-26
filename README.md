f33fe4a8b867316775144d118d2fbe7f8f04c75f
tecamenz <martin.camenzind@protonmail.ch>
2021-12-26 16:01:41 +0100

    updated documentation

--- README.md
+++ README.md
@@ -1,9 +1,54 @@
 # TimelapseVideoCreator
-Simple command line tool to create timelapse videos from images
+Simple command line tool to create timelapse videos from individual images. 
 
+Input images are sorted via EXIF timestamp to ensure correct order. 
+
+
+## Usage
+```cmd
+Usage: main.py [OPTIONS] [SRC] [DST]
+
+  Create timelapse from images located in SRC and saves the video to DST
+
+  If SRC contains subfolders, the program recursively scans all subfolders and
+  creates a video per subfolder. The folder structure at DSC will be
+  preserved, and the output videos are saved in respectively named folders.
+
+  If pre-processing via --average is set to True, the DST will contain
+  intermediate (averaged) images along with a output video.
+
+  If --novid is set to True along side with --average, only the intermediate
+  (averaged) images are saved. Useful if you like to create the final video in
+  a dedicated video editor.
+
+Options:
+  --window INTEGER   Defines the number of images to average. Default is 3.
+  --step INTEGER     Defines the number of images to step forward. Default is
+                     1.
+  --filetype TEXT    Defines the image filetype to search for.
+  --average          Option to average [window] images into one. Default is
+                     False.
+  --imscale INTEGER  Defines the width of the output image after averaging.
+                     Default is -1 which preserves native resolution.
+  --vscale INTEGER   Defines the width of the output video. Default is 1920.
+  --novid            If to omit video creation. Only used when --average ==
+                     True.
+  --localize TEXT    String for timezone conversion.
+  --ffmpeg_exe TEXT  Path to ffmpeg.exe.
+  --help             Show this message and exit.
+
+```
+
+### Example
+
+Create a 4k-Video with pre-processing using a window of 4 images and a step size of 2 (2 images overlap). 
+```cmd
+python main.py \path\to\images out --average True --window 4 --step 2 --vscale 4096 --ffmpeg_exe \ffmpeg-4.4.1-full_build\bin\ffmpeg.exe
+```
 
 ## Setup Conda Environment
 ```cmd 
 conda create -n timelapse python=3.9
 conda activate timelapse
+pip install -r requirements.txt
 ```
--- main.py
+++ main.py
@@ -37,17 +37,25 @@ def main(src, dst, window, step, filetype, average, imscale, vscale, novid, loca
 
 @click.command()
 @click.argument('src', default='/')
-@click.argument('dst', default='/')
-@click.option('--window', default=3, help='Defines the number of images to average. Default=3')
-@click.option('--step', default=1, help='Defines the number of images to step forward. Default=1')
-@click.option('--filetype', default='jpg', help='Defines the image filetype to search for')
-@click.option('--average', is_flag=True, default=False, help='Option to average [window] images into one')
-@click.option('--imscale', default=-1, help='Defines the width of the output image after averaging. -1 preserves native resolution.')
-@click.option('--vscale', default=1920, help='Defines the width of the output video')
-@click.option('--novid', is_flag=True, default=False, help='If to omit video creation. Only checkt when --average == True')
-@click.option('--localize', default=None, help='String for timezone conversion')
-@click.option('--ffmpeg_exe', default='/', help='Path to ffmpeg.exe')
+@click.argument('dst', default='out')
+@click.option('--window', default=3, help='Defines the number of images to average. Default is 3.')
+@click.option('--step', default=1, help='Defines the number of images to step forward. Default is 1.')
+@click.option('--filetype', default='jpg', help='Defines the image filetype to search for. Default is jpg')
+@click.option('--average', is_flag=True, default=False, help='Option to average [window] images into one. Default is False.')
+@click.option('--imscale', default=-1, help='Defines the width of the output image after averaging. Default is -1 which preserves native resolution.')
+@click.option('--vscale', default=1920, help='Defines the width of the output video. Default is 1920.')
+@click.option('--novid', is_flag=True, default=False, help='If to omit video creation. Only used when --average == True.')
+@click.option('--localize', default=None, help='String for timezone conversion.')
+@click.option('--ffmpeg_exe', default='/', help='Path to ffmpeg.exe.')
 def main_command(src, dst, window, step, filetype, average, imscale, vscale, novid, localize, ffmpeg_exe):
+    """Create timelapse from images located in SRC and saves the video to DST
+
+    If SRC contains subfolders, the program recursively scans all subfolders and creates a video per subfolder. The folder structure at DSC will be preserved, and the output videos are saved in respectively named folders. 
+    
+    If pre-processing via --average is set to True, the DST will contain intermediate (averaged) images along with a output video.
+    
+    If --novid is set to True along side with --average, only the intermediate (averaged) images are saved. Useful if you like to create the final video in a dedicated video editor.
+    """
     main(src, dst, window, step, filetype, average, imscale, vscale, novid, localize, ffmpeg_exe)
 
 
